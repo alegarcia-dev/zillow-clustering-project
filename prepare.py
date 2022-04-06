190b09e52788f4c699bf2a75a8855b03a32556bf
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-04-06 16:41:32 -0500

    :heavy_plus_sign: Added single unit filter.
    Changes to be committed:
    	modified:   prepare.py
    	modified:   wrangle.ipynb
    
    prepare: Added function for filtering out non-single unit properties
        based on the property type description.
    
    wrangle: Added step to initial preparation phase for removing any
        properties that are not single units based on the property type
        description.

--- prepare.py
+++ prepare.py
@@ -17,6 +17,7 @@
 #           summarize_row_nulls(df)
 #           prepare_zillow(df)
 #           drop_missing_values(df, prop_required_column = 0, prop_required_row = 0)
+#           feature_engineering(df)
 #
 #
 ################################################################################
@@ -48,7 +49,9 @@ def summarize_row_nulls(df):
 
 def prepare_zillow(df):
     df_copy = df.copy()
-    return drop_missing_values(df_copy, prop_required_column = 0.8, prop_required_row = 1)
+    df_copy = drop_missing_values(df_copy, prop_required_column = 0.8, prop_required_row = 1)
+    df_copy = get_single_unit_properties(df)
+    return df_copy
 
 ################################################################################
 
@@ -56,4 +59,35 @@ def drop_missing_values(df, prop_required_column = 0, prop_required_row = 0):
     df = df.dropna(axis = 'columns', thresh = round(df.shape[0] * prop_required_column))
     df = df.dropna(axis = 'index', thresh = round(df.shape[1] * prop_required_row))
     
+    return df
+
+################################################################################
+
+def get_single_unit_properties(df):
+    property_types = [
+        'Single Family Residential',
+        'Condominium',
+        'Cluster Home',
+        'Mobile Home',
+        'Manufactured, Modular, Prefabricated Homes',
+        'Residential General',
+        'Townhouse'
+    ]
+    df = df[df.propertylandusedesc.isin(property_types)]
+    
+    return df
+
+################################################################################
+
+def feature_engineering(df):
+    '''
+    Feature engineering
+    '''
+    df['sqft_per_bed'] = df['calculatedfinishedsquarefeet'] / df['bedroomcnt']
+    df['sqft_per_bath'] = df['calculatedfinishedsquarefeet'] / df['bathroomcnt']
+    df['total_rooms'] = df['bedroomcnt'] + df['bathroomcnt']
+    df['bed_bath_rooms_per_sqft_living'] = df['total_rooms'] / df['calculatedfinishedsquarefeet']
+    df['age_in_years'] = 2017 - df['yearbuilt']
+    df['taxrate'] = df['taxamount'] / df['taxvaluedollarcnt']
+    df['dollars_per_sqft'] = df['taxvaluedollarcnt'] / df['calculatedfinishedsquarefeet']
     return df
